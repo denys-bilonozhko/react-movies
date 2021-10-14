a0309f770a8a205d9ded871df181416ce782fe63
Denys Bilonozhko <deformix@gmail.com>
2021-10-14 20:59:31 +0300

    add layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
 
 const App = () => {
   return (
-    <div>
+    <div className="page">
       <Header />
+      <main className="main">text</main>
+      <Footer />
     </div>
   );
 };
