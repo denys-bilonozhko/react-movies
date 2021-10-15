e4b1df53d0edec915a86165ef182b2770920cfdd
Denys Bilonozhko <deformix@gmail.com>
2021-10-15 14:45:43 +0300

    remove api key

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import Search from './components/search/Search';
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
   useEffect(() => {
-    fetch(`https://www.omdbapi.com/?apikey=7b9ce72f&s=${'star wars'}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${'star wars'}`)
       .then((response) => response.json())
       .then((data) => {
         setMovies(data.Search);
@@ -19,7 +20,7 @@ const App = () => {
 
   const searchMovie = (str) => {
     setLoading(true);
-    fetch(`https://www.omdbapi.com/?apikey=7b9ce72f&s=${str}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}`)
       .then((response) => response.json())
       .then((data) => setMovies(data.Search));
     setLoading(false);
