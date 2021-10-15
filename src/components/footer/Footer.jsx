77bab66bb2954feb20c6910666ad88430d1c143b
Denys Bilonozhko <deformix@gmail.com>
2021-10-15 10:47:17 +0300

    className attribute fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer class="page-footer indigo darken-3">
-      <div class="footer-copyright">
-        <div class="container">
+    <footer className="page-footer indigo darken-3">
+      <div className="footer-copyright indigo darken-3">
+        <div className="container">
           © {new Date().getFullYear()} denys-b
-          <a class="grey-text text-lighten-4 right" href="./">
+          <a className="grey-text text-lighten-4 right" href="./">
             GitHub
           </a>
         </div>
