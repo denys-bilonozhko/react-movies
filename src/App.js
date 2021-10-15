5709b7cf35506dae9652cdd31b137f99a84b3fa8
Denys Bilonozhko <deformix@gmail.com>
2021-10-15 12:14:07 +0300

    app composition

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Header } from './components/header/Header';
-import { Footer } from './components/footer/Footer';
+import { useState, useEffect } from 'react';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Movies from './components/movies/Movies';
+import Search from './components/search/Search';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=7b9ce72f&s=${'star wars'}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+      });
+  }, []);
+
+  const searchMovie = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=7b9ce72f&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.Search));
+  };
+
   return (
     <div className="page">
       <Header />
-      <main className="main">text</main>
+      <Search searchMovie={searchMovie} />
+      <main className="main">
+        {movies.length ? (
+          <Movies movies={movies} />
+        ) : (
+          <p className="center">Loading...</p>
+        )}
+      </main>
       <Footer />
     </div>
   );
